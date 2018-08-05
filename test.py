45f690759fc224891098c9dba93e28312a038ddf
EG-easy <1018eguchi@gmail.com>
2018-08-05 15:23:46 +0900

    add new file

---
+++ test.py
@@ -0,0 +1,19 @@
+def pow_simple(a, n):
+    r = 1
+    p = 1
+    while p <= n:
+        r *= a
+        p += 1
+    return r
+
+
+a = 
+n =
+mod =
+
+value = pow_simple(a, n)
+answer = value % mod
+
+print(answer)
+
+
